ce70822fd6bb82af238845402dc79e75fcca1166
Rayzhuxy <734400huachuan@gmail.com>
2025-03-16 23:50:17 +0800

    addfinal

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,12 +1,15 @@
-import { defineNavbarConfig } from 'vuepress-theme-plume'
+import { defineNavbarConfig } from "vuepress-theme-plume";
 
 export const navbar = defineNavbarConfig([
-  { text: '首页', link: '/' },
-  { text: '博客', link: '/blog/' },
-  { text: '标签', link: '/blog/tags/' },
-  { text: '归档', link: '/blog/archives/' },
+  { text: "首页", link: "/" },
+  { text: "博客", link: "/blog/" },
+  { text: "标签", link: "/blog/tags/" },
+  { text: "归档", link: "/blog/archives/" },
   {
-    text: '笔记',
-    items: [{ text: '示例', link: '/notes/demo/README.md' }]
+    text: "笔记",
+    items: [
+      { text: "Demo", link: "/notes/demo/README.md" },
+      { text: "程序猿", link: "/notes/程序猿/README.md" },
+    ],
   },
-])
+]);
