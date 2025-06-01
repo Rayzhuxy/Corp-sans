a9a73af223311579975ff516e7ba8cabb1e5b219
Rayzhuxy <734400huachuan@gmail.com>
2025-06-01 18:13:45 +0800

    learn_cpp

--- docs/.vuepress/notes/cpp.ts
+++ docs/.vuepress/notes/cpp.ts
@@ -3,5 +3,19 @@ import { defineNoteConfig } from "vuepress-theme-plume";
 export default defineNoteConfig({
   dir: "cpp",
   link: "/cpp/",
-  sidebar: ["README.md", "test1"],
+  sidebar: [
+    "README.md",
+    {
+      text: "二、C++核心编程",
+      prefix: "core",
+      items: [
+        "1-内存分区模型.md",
+        "2-引用.md",
+        "3-函数提高.md",
+        "4-类和对象.md",
+        "5-文件操作.md",
+      ],
+    },
+    "test1",
+  ],
 });
